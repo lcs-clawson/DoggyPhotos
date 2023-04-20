5a4100f0253a9797dd8bb20141e11d048b89939a
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-20 08:42:46 -0400

    Initial Commit

---
+++ DoggyPhotos/DoggyPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoggyPhotosApp.swift
+//  DoggyPhotos
+//
+//  Created by Cooper Lawson on 2023-04-20.
+//
+
+import SwiftUI
+
+@main
+struct DoggyPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
