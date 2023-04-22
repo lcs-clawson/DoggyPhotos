cde35686d3f259865660220d62e9ef9b088951c3
Cooper Lawson <CSLawson@lcs.on.ca>
2023-04-22 19:02:11 -0400

    Deleted everything and went back to an earlier commit.

--- DoggyPhotos/Views/DoggyView.swift
+++ DoggyPhotos/Views/DoggyView.swift
@@ -4,69 +4,54 @@ struct DoggyView: View {
     
     @State var imageUrlOpacity = 0.0
     
-    @State var currentDoggy = Doggy?
+    @State var currentDoggy = exampleDoggy
     
     @State var imageUrl: String?
     
     var body: some View {
-        NavigationView{ }
         VStack{
+            Text("Doggy Photos")
+                .font(.largeTitle)
+                .foregroundColor(Color.blue)
+                .bold()
+            
+            Text("What will your doggy photo of the day be?")
+                .font(.footnote)
+                .fontWeight(.semibold)
             
             Spacer()
             
-            if let currentDoggy = currentDoggy {
-             
-                Text("Doggy Photos")
-                    .font(.largeTitle)
-                    .foregroundColor(Color.blue)
-                    .bold()
-                
-                Text("What will your doggy photo of the day be?")
-                    .font(.footnote)
-                    .fontWeight(.semibold)
-                
-                Spacer()
-                
-                Button(action: {
-                    withAnimation(.easeIn(duration: 1.0)) {
-                        // Set the image URL when the button is pressed
-                        imageUrl = currentDoggy.imageUrl
-                    }
-                }, label: {
-                    Image(systemName: "arrow.down.circle.fill")
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width:40)
-                        .tint(.black)
-                })
-                
-                Spacer()
-                
-                
-                if let url = imageUrl, let imageUrl = URL(string: url) {
-                    AsyncImage(url: imageUrl,
-                               content: { downloadedImage in
-                        downloadedImage
-                    },
-                               placeholder: {
-                        ProgressView()
-                    })
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity)
-                
-                } else {
-                    ProgressView()
-                    
+            Button(action: {
+                withAnimation(.easeIn(duration: 1.0)) {
+                    // Set the image URL when the button is pressed
+                    imageUrl = currentDoggy.imageURL
                 }
+            }, label: {
+                Image(systemName: "arrow.down.circle.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width:40)
+                    .tint(.black)
+            })
             
+            Spacer()
+            
+            
+            if let url = imageUrl, let imageUrl = URL(string: url) {
+                AsyncImage(url: imageUrl,
+                           content: { downloadedImage in
+                    downloadedImage
+                },
+                           placeholder: {
+                    ProgressView()
+                })
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
             }
             
         }
         .navigationTitle("Doggy Photos")
     }
-        .task {
-            currentDoggy = await NetworkService.fetch()
-        }
 }
 
 struct DoggyView_Previews: PreviewProvider {
@@ -74,3 +59,8 @@ struct DoggyView_Previews: PreviewProvider {
         DoggyView()
     }
 }
+
+    
+
+
+
--- DoggyPhotos/Views/Model/Doggy.swift
+++ DoggyPhotos/Views/Model/Doggy.swift
@@ -10,7 +10,9 @@ import Foundation
 struct Doggy: Codable {
     var breed: String
     var imageURL: String
+    var name: String
+    var age: Int
 }
 
-let exampleDoggy = Doggy(breed: "Spitz", imageURL: "https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_5989.jpg")
+let exampleDoggy = Doggy(breed: "Spitz", imageURL: "https://images.dog.ceo/breeds/schnauzer-miniature/n02097047_5989.jpg", name: "Frank", age: 3)
  
